a3bdf8b039cca9078351cdb69d71d5f95a396ffc
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-06-10 22:03:07 +0900

    initial commits

--- cmd/main.go
+++ cmd/main.go
@@ -2,123 +2,19 @@ package main
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 )
 
-type Room struct {
-	ID       string
-	Clients  map[*websocket.Conn]bool
-	Messages []string
-}
-
-func (r *Room) Broadcast(message string) {
-	r.Messages = append(r.Messages, message)
-	for client := range r.Clients {
-		err := client.WriteMessage(websocket.TextMessage, []byte(message))
-		if err != nil {
-			log.Println("Failed to send message to client:", err)
-			client.Close()
-			delete(r.Clients, client)
-		}
-	}
-}
-
-// func (r *Room) Broadcast(message string) {
-// 	r.Messages = append(r.Messages, message)
-
-// 	userCount := len(r.Clients)
-
-// 	for client := range r.Clients {
-// 		err := client.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("message:%s,userCount:%d", message, userCount)))
-// 		if err != nil {
-// 			log.Println("Failed to send message to client:", err)
-// 			client.Close()
-// 			delete(r.Clients, client)
-// 		}
-// 	}
-// }
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
 func main() {
-	router := gin.Default()
-	router.Use(cors.Default())
+	log.Println("Starting server...")
 
-	rooms := make(map[string]*Room)
-	roomIDs := []string{"room1", "room2"}
-	for _, id := range roomIDs {
-		rooms[id] = &Room{
-			ID:       id,
-			Clients:  make(map[*websocket.Conn]bool),
-			Messages: []string{},
+	// Load env from .env file
+	if gin.Mode() != gin.ReleaseMode {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Error loading .env file")
 		}
 	}
-
-	router.GET("/ws/:roomID", func(c *gin.Context) {
-		roomID := c.Param("roomID")
-		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-		if err != nil {
-			log.Println("Failed to upgrade connection:", err)
-			return
-		}
-
-		room, exists := rooms[roomID]
-		if !exists {
-			log.Println("Invalid room:", roomID)
-			conn.Close()
-			return
-		}
-
-		if len(room.Clients) >= 2 {
-			log.Println("Room is full")
-			conn.Close()
-			return
-		}
-
-		room.Clients[conn] = true
-
-		for _, message := range room.Messages {
-			err := conn.WriteMessage(websocket.TextMessage, []byte(message))
-			if err != nil {
-				log.Println("Failed to send message to client:", err)
-				conn.Close()
-				delete(room.Clients, conn)
-				return
-			}
-		}
-
-		go func() {
-			for {
-				_, msg, err := conn.ReadMessage()
-				if err != nil {
-					log.Println("Failed to read message from client:", err)
-					conn.Close()
-					delete(room.Clients, conn)
-					return
-				}
-				room.Broadcast(string(msg))
-			}
-		}()
-	})
-
-	router.GET("/api/rooms/:roomID/users", func(c *gin.Context) {
-		roomID := c.Param("roomID")
-
-		room, exists := rooms[roomID]
-		if !exists {
-			log.Println("Invalid room:", roomID)
-			c.JSON(http.StatusNotFound, gin.H{"error": "Invalid room"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"count": len(room.Clients)})
-	})
-
-	router.Run(":8080")
 }
--- go.mod
+++ go.mod
@@ -2,27 +2,24 @@ module github.com/sinde530/go-mancer
 
 go 1.18
 
-require (
-	github.com/gin-gonic/gin v1.9.1
-	github.com/gorilla/websocket v1.5.0
-)
+require github.com/gin-gonic/gin v1.9.1
 
 require (
-	github.com/gofrs/uuid v4.0.0+incompatible // indirect
-	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
-	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/googollee/go-socket.io v1.7.0
+	github.com/joho/godotenv v1.5.1
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
