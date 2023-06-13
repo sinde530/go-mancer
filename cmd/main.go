4b81ceb0fd9684dfc2bfc082b99038d09f5f0839
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-06-13 10:31:42 +0900

    Add ip tracker

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/gin-contrib/cors"
@@ -36,6 +42,7 @@ func main() {
 	r.POST("/account/login", handler.HandleLogin)
 	r.POST("/account/logout", handler.HandleLogout)
 	r.POST("/refresh", token.TokenChange)
+	r.GET("/iptrack/:ip", IPTrack)
 
 	port := os.Getenv("PORT")
 	log.Fatal(r.Run(":" + port))
@@ -44,5 +51,56 @@ func main() {
 func HandleTest(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "hi",
+	})
+}
+
+func IPTrack(c *gin.Context) {
+	ip := c.Param("ip")
+
+	parsedIP := net.ParseIP(ip)
+	if parsedIP == nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid IP address",
+		})
+		return
+	}
+
+	// IP 위치 정보 조회
+	apiURL := fmt.Sprintf("http://ip-api.com/json/%s", url.PathEscape(ip))
+	response, err := http.Get(apiURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// JSON 파싱
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if result["status"] == "fail" {
+		c.JSON(500, gin.H{
+			"error": "Failed to retrieve location information",
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"country_code": result["countryCode"],
+		"city":         result["city"],
+		"latitude":     result["lat"],
+		"longitude":    result["lon"],
+		"isp":          result["isp"],
+		"mobile":       result["mobile"],
+		"as":           result["as"],
+		"proxy":        result["proxy"],
+		"hosting":      result["hosting"],
 	})
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/oschwald/maxminddb-golang v1.10.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -37,6 +38,7 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/oschwald/geoip2-golang v1.8.0
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/sethvargo/go-envconfig v0.9.0
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
